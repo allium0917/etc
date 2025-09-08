0c81ec58c77cba7eab4325458c03f50f7c0ab4e3
allium0917 <sistus0917@gmail.com>
2025-09-08 14:19:35 +0900

    0908

---
+++ c++study/dndhkd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char Char;
+    cin >> Char;
+    int ascii = static_cast<int>(Char);
+
+    cout <<  ascii << endl;
+
+    return 0;
+}
