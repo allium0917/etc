ab6311d97c6108a990f92cfd03c1d92fbd60538e
allium0917 <sistus0917@gmail.com>
2025-09-11 23:53:45 +0900

    백준 펑되었나..

---
+++ c++study/28235.cpp
@@ -0,0 +1,24 @@
+// 구호 SONGDO에 대해 HIGHSCHOOL로 응원.
+// 구호 CODE에 대해 MASTER로 응원.
+// 구호 2023에 대해 0611로 응원.
+// 구호 ALGORITHM에 대해 CONTEST로 응원.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if (s=="SONGDO") {
+        cout << "HIGHSCHOOL" << endl;
+    }
+    else if (s=="CODE") {
+        cout << "MASTER" << endl;
+    }
+    else if (s=="2023") {
+        cout << "0611" << endl;
+    }
+    else if (s=="ALGOLITHM") {
+        cout << "CONTEST" << endl;
+    }
+}
